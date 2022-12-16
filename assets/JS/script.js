fef94082d21ccd63c955339625cc26cf39ecf46e
PaulKnees <jakewallace368@gmail.com>
2022-12-16 20:23:14 +0000

    appends SOME info to todays weather

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,37 +3,48 @@ var APIkey = 'd7d17ff04f8715fd79b80f173e2710a1'
 var searchInput = $('.weather-search');
 var searchForm = $('#search-form');
 var searchBtn = $('.search-button');
+var todaySec = $('#today');
+var forecastSec = $('#forecast');
 
 // var searchText = 'london'
 
 
-searchBtn.click(function(event){
+searchBtn.click(function (event) {
     event.preventDefault();
-    console.log('click')
     var searchText = searchInput.val().trim();
     console.log(searchText)
-    if (!searchText){
+    if (!searchText) {
         alert('please type a city name');
     } else {
 
         $.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${APIkey}`)
-        .then(function(data) {
-            // console.log(`
-            // temp: ${Math.round(data.main.temp)}
-            // Wind Speed: ${data.wind.speed}
-            // Humidity: ${data.main.humidity}%
-            // `);
-            console.log(data.coord.lon, 'lon', data.coord.lat, 'lat')
-        
-            $.get( `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${APIkey}`)
-        .then(function(FCData){
-            console.log(FCData, 'FCData')
-        }
-        )
-        })
+            .then(function (data) {
+                /////////////////////////////////////////////////////////////////////////////////////Need to utilise moment.js to get accurate, current readings
+                
+                todaySec.append(`<div class="card" id="todaysWeather">
+                <h2 class="name">${data.name}</h2>
+               <h4 id="todayTemp">Temperature:</h4>
+               <h4 id="todayWind">Wind:</h4>
+               <h4 id="todayTempumidity">Humidity:</h4>
+                <h4 id="sunTimes">Sunrise: ${data.sunrise}, Sunset: ${data.sunset}</h4>
+             </div>`)
+                // console.log(`
+                // temp: ${Math.round(data.main.temp)}
+                // Wind Speed: ${data.wind.speed}
+                // Humidity: ${data.main.humidity}%
+                // `);
+                console.log(data.name, 'name')
+                console.log(data.coord.lon, 'lon', data.coord.lat, 'lat')
+
+                $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${APIkey}`)
+                    .then(function (FCData) {
+                        console.log(FCData, 'FCData')
+                    }
+                    )
+            })
 
     }
-        
+
 })
 
 // }
